93f8cd3cd7d87e481ad24af9766d3ecf0a772cf2
xinming Feng <ucfnfen@ucl.ac.uk>
2024-12-06 15:52:27 +0000

    fixed

--- WS03/Assets/Resources/LineChartUpdater.cs
+++ WS03/Assets/Resources/LineChartUpdater.cs
@@ -1,29 +1,25 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XCharts.Runtime;
+
 
 public class LineChartUpdater : MonoBehaviour
 {
     public LineChart timeDomain;
     private int maxDataPoints = 10;
+    private int startX = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         timeDomain = GetComponent<LineChart>();
-        var serie = Line.AddDefaultSerie(timeDomain, "Time Domain");
+        Line.AddDefaultSerie(timeDomain, "Time Domain");
+        timeDomain.SetMaxCache(maxDataPoints);
     }
 
     public void AppendData(int value)
     {
-        var serie = timeDomain.GetSerie("Time Domain");
-        timeDomain.AddData(serie.index, value);
-        if (timeDomain.GetSerie("Time Domain").dataCount > maxDataPoints)
-        {
-            serie.RemoveData(0);
-        }
-        timeDomain.RefreshChart();
+        timeDomain.AddData(0, startX, value);
+        startX++;
     }
 }
